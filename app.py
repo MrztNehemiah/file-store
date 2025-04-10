af130957884ff166da996abc98e91e3dd1b2dc98
Nehemiah <danielnehemiah15@gmail.com>
2025-04-10 08:19:30 +0100

    Add basic file upload to S3 functionality using Flask and Boto3

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+import os
+import boto3
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-	return "Flask server is runnung on EC2"
+# AWS setup
+S3_BUCKET = "nehesbucket"
+s3 = boto3.client('s3')
 
-if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=80)
+@app.route('/')
+def index():
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return 'No file part'
+    
+    file = request.files['file']
+    
+    if file.filename == '':
+        return 'No selected file'
+    
+    s3.upload_fileobj(
+        file,
+        S3_BUCKET,
+        file.filename,
+        ExtraArgs={'ACL': 'public-read'}  # Optional: makes file public
+    )
+    
+    return f'File uploaded to S3: https://{S3_BUCKET}.s3.amazonaws.com/{file.filename}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
+
